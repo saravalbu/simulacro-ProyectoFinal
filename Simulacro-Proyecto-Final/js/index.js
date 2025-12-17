9bacb49519d567d5b34866699b8632c77ad53ec9
saravalbu <saravalbu@alumnos.cei.es>
2025-12-17 11:15:37 +0100

    Add files via upload

---
+++ Simulacro-Proyecto-Final/js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('scroll', () => {
+    const bannerShade = document.getElementById('bannerShade')
+
+    const overlay = document.getElementById('overlay')
+
+    const height = window.innerHeight / 4
+
+    if (pageYOffset >= height) {
+        
+        overlay.style.opacity = '0.6'
+    }
+    console.log(pageYOffset + ' de ' + window.innerHeight / 4)
+})
